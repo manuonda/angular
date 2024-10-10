59daf61e709ea971f8188364267a9e5ebaa71619
david garcia <david.garcia@grupomost.com>
2024-10-10 16:52:40 -0300

    task toolbar

---
+++ tutorials/signal-input/src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment'
+
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(), 
+    provideFirebaseApp(() => initializeApp(environment.firebase)), 
+    provideFirestore(() => getFirestore())]
+};
---
+++ tutorials/task-example-back-front/ng-task-18/src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './toolbar.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolbarComponent {
+
+}
