40dcbcefe5f3005eafd378d120042102ee3ad5e6
david garcia <david.garcia@grupomost.com>
2024-10-10 16:13:10 -0300

    init project angular

---
+++ tutorials/signal-input/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    firebase:{"projectId":"crud-angular-18-manuonda","appId":"1:627106400921:web:cb5ebdd3b02684248d0608","storageBucket":"crud-angular-18-manuonda.appspot.com","apiKey":"AIzaSyC3VbcSwT3CGOt6BB9xMf33CAUjmxlu0-g","authDomain":"crud-angular-18-manuonda.firebaseapp.com","messagingSenderId":"627106400921"}
+};
