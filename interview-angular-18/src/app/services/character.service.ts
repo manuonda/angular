0bfd1bd03a8939a00e5bb343c00c88a607ee9130
david garcia <david.garcia@grupomost.com>
2024-10-26 14:53:36 -0300

    add global store

--- interview-angular-18/src/app/services/character.service.ts
+++ interview-angular-18/src/app/services/character.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { CharacterAdapter } from '@app/adapters/character.adapter';
 import { Character, CharacterInfo } from '@app/models';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +19,34 @@ export class CharacterService {
     .pipe(map( info =>  CharacterAdapter(info)));
   }
 
-  addCharacter(character: Omit<Character, "id">) :Observable<Character[]>{
-    return this.http.get<CharacterInfo>(this.baseUrl)
-    .pipe(map(info => CharacterAdapter(info)));
+  addCharacter(character: Omit<Character, "id">) :Observable<void>{
+    return this.http.post<void>(this.baseUrl,{character})
+    .pipe(
+      catchError(() => {
+        console.info("error prevented for testing")
+        return Promise.resolve();
+      })
+    );
+  }
+
+  removeCharacter(id: number) :Observable<void>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<void>(url)
+    .pipe(
+      catchError(() => {
+        console.info("error prevented for testing")
+        return Promise.resolve();
+      })
+    );
+  }
+  updateCharacter(id: number) :Observable<void>{
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<void>(url)
+    .pipe(
+      catchError(() => {
+        console.info("error prevented for testing")
+        return Promise.resolve();
+      })
+    );
   }
 }
---
+++ interview-angular-18/src/app/store/global.store.ts
@@ -0,0 +1,73 @@
+import { inject, InjectionToken } from "@angular/core";
+import { Character } from "@app/models"
+import { CharacterService } from "@app/services";
+import {patchState, signalStore, withMethods, withState} from "@ngrx/signals"
+import { lastValueFrom } from "rxjs";
+import { addEntity, removeEntities, removeEntity, updateEntity, withEntities } from '@ngrx/signals/entities';
+
+type StoreState = {
+    characters: Character[];
+}
+
+const initialState: StoreState = {
+    characters : []
+}
+
+const STORE_STATE= new InjectionToken<StoreState>("GlobalStore",{
+    factory: () => initialState
+});
+
+export const GlobalStore = signalStore(
+{ providedIn: "root"},
+  withState(() => inject(STORE_STATE)),
+  withEntities<Character>(),
+  withMethods((store, characterService = inject(CharacterService)) => ({
+    getCharacter(id:number){
+      return store.characters().find(character => character.id === id)  
+    },
+    async addCharacter(character: Omit<Character, "id">){
+        try {
+            await lastValueFrom(characterService.addCharacter(character)) 
+            // patchState(store,({characters}) => ({
+            //     characters:[...characters,{
+            //         id: new Date().getTime(),
+            //         ...character
+            //     }]
+            //   })
+            // )
+            patchState(store, addEntity({
+                id: new Date().getTime(),
+                ...character
+            }))
+        } catch (error) {
+            
+        }
+    },
+    async removeCharacter(id:number){
+        try {
+            await lastValueFrom(characterService.removeCharacter(id))
+            /*patchState(store,({characters}) => ({
+                characters: [...characters.filter(char => char.id !== id)]
+              })
+            )*/
+           patchState(store, removeEntity(id));
+        } catch (error) {
+            
+        }
+    },
+    async updateCharacter(updateCharacter: Character){
+        try {
+            await lastValueFrom(characterService.addCharacter(updateCharacter))
+            /*
+            patchState(store,({characters}) => ({
+                characters: [...characters.map((char) => char.id === updateCharacter.id? { ...char,...updateCharacter}: char)]
+              })
+            ) */
+           patchState(store, updateEntity({ id: updateCharacter.id , changes: {...updateCharacter} }))
+
+        } catch (error) {
+            
+        }
+    }
+  }))
+)
