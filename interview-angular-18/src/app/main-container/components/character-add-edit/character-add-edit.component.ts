f8043de478c4f8d08fc30eb09aae60d6d1409de7
david garcia <david.garcia@grupomost.com>
2024-10-29 23:55:15 -0300

    Add custom input

--- interview-angular-18/src/app/main-container/components/character-add-edit/character-add-edit.component.ts
+++ interview-angular-18/src/app/main-container/components/character-add-edit/character-add-edit.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
-//
+import { ChangeDetectionStrategy, Component, computed, inject, input, Signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+
+import { emptyCharacter } from '@app/models';
 import { GlobalStore } from '@app/store';
+import {CustomInputComponent} from '@app/components'
 
 
 interface CharacterForm{
@@ -13,13 +15,48 @@ interface CharacterForm{
 @Component({
   selector: 'app-character-add-edit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule ,CustomInputComponent],
   templateUrl: './character-add-edit.component.html',
   styleUrl: './character-add-edit.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharacterAddEditComponent {
+  //directamente obtengo este seria el id como 
+  //parametro
   id = input<number>();
-
   readonly store = inject(GlobalStore)
+  
+  characterToEdit = computed( () => 
+      this.store.getCharacter(Number(this.id())) ?? emptyCharacter
+  );
+  characterForm: Signal<FormGroup> = computed (
+   () => new FormGroup<CharacterForm>({
+     name: new FormControl(this.characterToEdit().name ,{
+      nonNullable: true,
+      validators:[Validators.required]
+     }),
+     image: new FormControl(this.characterToEdit().image,{
+      nonNullable: true,
+      validators: [Validators.required]
+     })
+   })
+  );
+
+
+  onSubmit():void{
+    if (this.characterForm().valid){
+      const character = {
+        ...(this.id() ? { id: Number(this.id())}: {}),
+        ...this.characterForm().value
+      }
+    
+      const methodToUse = this.id()? "updateCharacter":"addCharacter";
+
+      this.store[methodToUse](character);
+      this.characterForm().reset();  
+    
+    }
+
+  }
+
 }
