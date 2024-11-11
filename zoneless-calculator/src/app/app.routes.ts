5c10c6c143be22c1be0afbae57703b5d2f8c6be8
David Garcia <david.garcia@grupomost.com>
2024-11-11 20:52:50 -0300

    add files

--- zoneless-calculator/src/app/app.routes.ts
+++ zoneless-calculator/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path:'calculator',
+     loadComponent:() => import("@app/calculator/views/calculator-view/calculator-view.component").then( m => m.CalculatorViewComponent),
+    },
+    {
+        path:'**',
+        redirectTo: 'calculator'
+    }
+];
