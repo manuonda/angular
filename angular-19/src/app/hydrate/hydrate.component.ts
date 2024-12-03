885d0e70625ad89b5a149e37fa6a05358eb54d43
david garcia <david.garcia@grupomost.com>
2024-12-02 23:39:38 -0300

    Add example hydrate component

--- angular-19/src/app/hydrate/hydrate.component.ts
+++ angular-19/src/app/hydrate/hydrate.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { booleanAttribute, Component, inject, input, output, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-hydrate',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './hydrate.component.scss'
 })
 export class HydrateComponent {
+
+  isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
+  name =input.required<string>({
+    alias:'title',
+    transform: toCapitalize,
+  })
+  isAdmin = input<boolean, boolean>(false,{
+    transform: booleanAttribute
+  });
+
+  messageEvent = output<string>();
+
+  sendMessage(){
+    this.messageEvent.emit('Hello from child');
+  }
+  constructor(){
+    if( this.isBrowser) {
+      localStorage.setItem("key","test")
+    }
+   }
+
 
 }
