49959f87f0e4c9b69fe34f043d684128c299288a
david garcia <david.garcia@grupomost.com>
2024-12-04 16:08:26 -0300

    add project testing

---
+++ curso-angular-moderno-udemy-main/src/app/features/checkout/checkout.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+
+
+const routes: Routes = [
+    {
+        path:'',
+        loadComponent: () => import('./checkout.component')
+    }
+]
+
+export default routes;
